c7490ef5094b51032e71211206b1b02ab0577de4
Dexter-Esith <gegiesitashvili@gmail.com>
2021-03-08 02:41:16 +0400

    adad

--- pyhot/hotelstar/hotel/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . models import Hotel, Contact, HotelReview
-# Register your models here.
-admin.site.register(Hotel)
-admin.site.register(Contact)
-admin.site.register(HotelReview)
