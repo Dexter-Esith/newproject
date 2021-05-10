4bf6b3f17d896d8d914318d3b44a43a3b7575944
Dexter-Esith <gegiesitashvili@gmail.com>
2021-05-10 21:05:49 +0400

    dft

--- Hotel/hotelstar/hotelstar/urls.py
+++ Hotel/hotelstar/hotelstar/urls.py
@@ -18,8 +18,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hotel.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
